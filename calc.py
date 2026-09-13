58f17715d22a5686ba6f225d7d6f8b2c2a3e8297
Eren <shuvamm939@gmail.com>
2026-09-13 23:52:42 +0530

    XD

---
+++ calc.py
@@ -0,0 +1,9 @@
+import  math
+import os
+HISTORY_FILE = "calculator_history.txt"
+history = []
+memory = 0 #Real calculator's M button
+
+#____________XD CALCULATOR____________________
+
+def calculate(expression):
